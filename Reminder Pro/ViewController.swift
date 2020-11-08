2c043f4503958dfcc2340c3e043f646cb39f9f56
Jayniel Singh <jaynielsingh@yahoo.com>
2020-11-07 21:53:32 -0800

    Initial Commit

---
+++ Reminder Pro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder Pro
+//
+//  Created by Jayniel on 11/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
